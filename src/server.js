a060968cd3ee8d6396adcba3b02cbd2f16285ac6
Espadv69 <aitorjob69@gmail.com>
2025-02-08 13:03:29 +0100

    Add routes import and remove temporal API

--- src/server.js
+++ src/server.js
@@ -1,11 +1,8 @@
 import express from 'express'
+import userRoutes from './routes/userRoutes.js'
 
 const app = express()
 const port = process.env.PORT || 3000
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello, world! This is a simple API' })
-})
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
