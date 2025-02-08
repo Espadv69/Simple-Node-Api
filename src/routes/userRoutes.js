890b3898df6b69a0751752fabe87962fadc021e4
Espadv69 <aitorjob69@gmail.com>
2025-02-08 13:42:32 +0100

    Get an user per ID improved

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -13,4 +13,15 @@ router.get('/', (req, res) => {
   res.json(users)
 })
 
+// Get an user per ID (GET /users/:id)
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+  const user = users.find((u) => u.id === parseInt(id))
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' })
+  }
+  res.json(user)
+})
+
 export default router
