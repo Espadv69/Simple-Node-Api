d12e25b27810d9d1a425ed511d7c47baa7a6f1c9
Espadv69 <aitorjob69@gmail.com>
2025-02-08 13:09:58 +0100

    Add root route

--- src/server.js
+++ src/server.js
@@ -4,6 +4,11 @@ import userRoutes from './routes/userRoutes.js'
 const app = express()
 const port = process.env.PORT || 3000
 
+// Root route '/'
+app.get('/', (req, res) => {
+  res.send('Welcome to the simple Node API!')
+})
+
 // Middleware to process JSON
 app.use(express.json())
 
