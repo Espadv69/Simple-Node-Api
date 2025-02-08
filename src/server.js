f9bf95765637fbb5fde98cd9eb3a800c5fee5a23
Espadv69 <aitorjob69@gmail.com>
2025-02-08 13:04:45 +0100

    Add middleware to precess JSON and Routes

--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ import userRoutes from './routes/userRoutes.js'
 const app = express()
 const port = process.env.PORT || 3000
 
+// Middleware to process JSON
+app.use(express.json())
+
+// Routes
+app.use('/users', userRoutes)
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
