1da0ea6eaeaa209500eb934b9a6ae938c5336bfe
Espadv69 <aitorjob69@gmail.com>
2025-02-08 12:59:11 +0100

    Get all users

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,3 +1,10 @@
 import express from 'express'
 
 const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.json([
+    { id: 1, name: 'Aitor' },
+    { id: 2, name: 'Vanesa' },
+  ])
+})
