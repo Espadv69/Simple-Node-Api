1788a8be45173d6411380f793547c5b0b16f2a46
Espadv69 <aitorjob69@gmail.com>
2025-02-08 13:37:50 +0100

    Remove to rebuild another thing

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,18 +2,4 @@ import express from 'express'
 
 const router = express.Router()
 
-// Get all users (GET /users)
-router.get('/', (req, res) => {
-  res.json([
-    { id: 1, name: 'Aitor' },
-    { id: 2, name: 'Vanesa' },
-  ])
-})
-
-// Get an user per ID (GET /users/:id)
-router.get('/:id', (req, res) => {
-  const { id } = req.params
-  res.json({ id, name: `User: ${id}` })
-})
-
 export default router
