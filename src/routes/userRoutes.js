6fb367b8a547e5211eead9ee45bd6d6c14ad3bed
Espadv69 <aitorjob69@gmail.com>
2025-02-08 13:01:32 +0100

    Get an user per ID

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,9 +2,18 @@ import express from 'express'
 
 const router = express.Router()
 
+// Get all users (GET /users)
 router.get('/', (req, res) => {
   res.json([
     { id: 1, name: 'Aitor' },
     { id: 2, name: 'Vanesa' },
   ])
 })
+
+// Get an user per ID (GET /users/:id)
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+  res.json({ id, name: `User: ${id}` })
+})
+
+export default router
