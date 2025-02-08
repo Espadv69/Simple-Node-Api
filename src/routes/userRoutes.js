59df6a9c85ff4cad95f261b733918120a3858dca
Espadv69 <aitorjob69@gmail.com>
2025-02-08 13:39:02 +0100

    Add variaable that simulated a database

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,4 +2,10 @@ import express from 'express'
 
 const router = express.Router()
 
+// Simulated DataBase
+let users = [
+  { id: 1, name: 'Aitor' },
+  { id: 2, name: 'Alice' },
+]
+
 export default router
