e2eebff7bff5cb75deb769581aa54359489380de
Espadv69 <aitorjob69@gmail.com>
2025-02-09 11:38:49 +0100

    Add addEventListener to form and preventDefault to not reload the page

--- public/script.js
+++ public/script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Handle form submit to create a new user
   const $form = document.querySelector('.create-user-form')
+  $form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const $name = document.querySelector('.user-name')
+  })
 })
