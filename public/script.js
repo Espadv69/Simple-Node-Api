ca9a089d8543c08991804a4aeda55d3b0be90b59
Espadv69 <aitorjob69@gmail.com>
2025-02-09 11:41:28 +0100

    Add async createUser function toDo

--- public/script.js
+++ public/script.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Get all users when page is loaded
-  fetchUsers() // Todo
+  fetchUsers() // ToDo
 
   // Handle form submit to create a new user
   const $form = document.querySelector('.create-user-form')
   $form.addEventListener('submit', (event) => {
     event.preventDefault()
     const $name = document.querySelector('.user-name').value
+    createUser($name) // ToDo
   })
 })
