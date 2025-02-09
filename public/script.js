5ededa47867b52c5bc3bb6077bb1a607e1d9f252
Espadv69 <aitorjob69@gmail.com>
2025-02-09 11:46:39 +0100

    fetchUsers toDo done, message was removed

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Get all users when page is loaded
-  fetchUsers() // ToDo
+  fetchUsers()
 
   // Handle form submit to create a new user
   const $form = document.querySelector('.create-user-form')
