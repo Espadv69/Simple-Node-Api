5b06e3dcc23501bf26efe01b75d2cdb643d6ccb0
Espadv69 <aitorjob69@gmail.com>
2025-02-09 11:46:01 +0100

    feat: add function to fetch and display users from API

--- public/script.js
+++ public/script.js
@@ -9,4 +9,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const $name = document.querySelector('.user-name').value
     createUser($name) // ToDo
   })
+
+  // Function to get all users from the API
+  async function fetchUsers() {
+    const response = await fetch('/users')
+    const users = await response.json()
+
+    const $userList = document.querySelector('.users-list')
+    $userList.innerHTML = '' // Clean up before add new users
+
+    users.forEach(user => {
+      const $li = document.createElement('li')
+      $li.textContent = `${user.name} (ID: ${user.id})`
+      $userList.appendChild($li)
+    })
+  }
 })
